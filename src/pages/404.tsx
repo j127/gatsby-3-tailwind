cc05e31bd8012aac19a0a6c153ab51305b5251ec
J. Cohen <hostelmarketing@gmail.com>
2021-05-19 16:50:08 -0700

    Change to typescript

---
+++ src/pages/404.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+const NotFoundPage: React.FC = (): JSX.Element => {
+    return (
+        <main>
+            <h1>Not Found</h1>
+        </main>
+    );
+};
+
+export default NotFoundPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const IndexPage: React.FC = (): JSX.Element => {
+    return <main>hello world</main>;
+};
+
+export default IndexPage;
