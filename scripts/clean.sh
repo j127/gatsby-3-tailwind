493ad25fb8cca31b28642597aa1145a33bcdb1c6
J. Cohen <hostelmarketing@gmail.com>
2021-05-19 18:21:12 -0700

    Remove makefile

---
+++ scripts/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Clean out junk files
+
+find . -name '*.pyc' -exec rm -f {} +
+find . -name '*.pyo' -exec rm -f {} +
+find . -name '*~' -exec rm -f {} +
+find . -name '__pycache__' -exec rm -rf {} +
